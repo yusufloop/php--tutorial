188510a38a85b4e0c860c89b78addc116d507b87
yusuf1 <yusufsmik@gmail.com>
2024-02-06 21:57:47 +0800

    part 4

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1>
-        <?php echo 'Hellow World'; ?>
+        <?php 
+        $greeting = "Hello";
+
+        echo "$greeting Everybody!";
+        ?>
     </h1>
 </body>
+
 </html>
